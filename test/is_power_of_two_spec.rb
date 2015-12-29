7f352532ef5993679ed470ddb0f9714ef8c77c55
Wiriya Rungruang <wiriya.run@gmail.com>
2015-12-30 03:42:20 +0700

    add testcase to 231_Power_of_Two

--- test/is_power_of_two_spec.rb
+++ test/is_power_of_two_spec.rb
@@ -2,9 +2,34 @@ require 'rspec'
 require_relative '../src/231_Power_of_Two'
 
 describe '#is_power_of_two?' do
-  context 'with check input is power of 2' do
-    it{
-      expect(is_power_of_two(2)).to eq(false)
-    }
+  context 'with input >= 2' do
+    it 'result is true' do
+      expect(is_power_of_two(2)).to eq(true)
+    end
+    it 'result is true' do
+      expect(is_power_of_two(4)).to eq(true)
+    end
+    it 'result is true' do
+      expect(is_power_of_two(512)).to eq(true)
+    end
+    it 'result is false' do
+      expect(is_power_of_two(1000)).to eq(false)
+    end
+  end
+
+  context 'with input less than 2' do
+    it 'negative input result is false' do
+      expect(is_power_of_two(-5)).to eq(false)
+    end
+    it 'input is zero then result is false' do
+      expect(is_power_of_two(0)).to eq(false)
+    end
+    it 'result is false' do
+      expect(is_power_of_two(1)).to eq(true)
+    end
+  end
+
+  context 'with zero' do
+
   end
 end
